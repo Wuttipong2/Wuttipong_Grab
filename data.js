6a2364b6e2dac290ca4080b2ef16a2ca4d45c43f
wuttipong phongam <614259046@webmail.npru.ac.th>
2020-10-07 22:12:30 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,78 @@
+const data =[
+   
+    {
+        "id" : 1,
+        "name" : "ข้าวต้มเจ๊เล็ก & เจ๊หมวย - ประชาสงเคราะห์",
+        "type" : "Coupon, Partner, อาหารทะเล, ของทอด, อาหารอีสาน",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000000we/hero/704dedf87eee4eb39281c798cc0855ec_1560937861498252104.jpeg"
+    },
+    {
+        "id" : 2,
+        "name" : "McDonald's (แมคโดนัลด์) - ราชดำเนิน",
+        "type" : "Partner, แฮมเบอร์เกอร์, ฟาสต์ฟู้ด, ไก่ทอด",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/AWh64KYIZXYdMpch2Zem/hero/91b61aa7c36445a19084dc93c77ea4c8_1600008272646540212.jpeg"
+    },
+    {
+        "id" : 3,
+        "name" : "Baan Nom Rang Nam (บ้านนม รางน้ำ) - ถนนรางน้ำ",
+        "type" : "Partner, ขนมปัง/เบเกอรี่/เค้ก",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000002k8/hero/41600b213489422cac41a317f2b7922a_1577024456475813353.jpeg"
+    },
+    {
+        "id" : 4,
+        "name" : "กูโรตีชาชัก (Goo Roti & Tea) - อนุสาวรีย์",
+        "type" : "Partner, กาแฟ/ชา, ชานมไข่มุก, ขนมปัง/เบเกอรี่/เค้ก",
+        "imageURL" : "https://dtlscuh0h90jk.cloudfront.net/seller/photos/45_964.jpg"
+    },
+    {
+        "id" : 5,
+        "name" : "ป้าเพ็ญ อาหารตามสั่ง - ซอยจรัญสนิทวงศ์",
+        "type" : "Partner, อาหารตามสั่ง",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZNYMA4FTFJ3A2/hero/ac1ac32814f542d198c24856fd25db27_1599581259458666991.jpeg"
+    },
+    {
+        "id" : 6,
+        "name" : "ไอติมทอด - ซอยราชดำเนินกลาง",
+        "type" : "Partner, ทานเล่น/ขนมขบเคี้ยว",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZNTJCKZEF3WE2/hero/624c2f0e8ba1423a9ceec6342fa4df7c_1600078054197205525.jpeg"
+    },
+    {
+        "id" : 7,
+        "name" : "เก๋อาหารตามสั่ง - ถนนมหาจักร",
+        "type" : "Coupon, Partner, อาหารตามสั่ง",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZEXHF3KERJFGA/hero/da744dc7c52c481aa383aed9cd16b0b6_1589532114847970839.jpeg"
+    },
+    {
+        "id" : 8,
+        "name" : "Jones' Salad (โจนส์สลัด) -โรงพยาบาลศิริราช",
+        "type" : "Coupon, Partner, อาหารสุขภาพ",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTEEN2TPCNACT/hero/9e867c07c59a499fad5d5c2bd00d3ac5_1598195187208581539.jpg"
+    },
+    {
+        "id" : 9,
+        "name" : "ไก่ไม่ยอมนอน - ซอยรามบุตรี",
+        "type" : "Coupon, Partner, ข้าวหน้า",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png"
+    },
+    {
+        "id" : 10,
+        "name" : "Kamu Tea (คามุ ที) - วังหลัง",
+        "type" : "Partner, ชานมไข่มุก",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZC1VN5CBEXGAT/hero/2cac19cd03f84037a8046cd3d9a30b85_1585793335979199515.jpeg"
+    },
+
+    {
+        "id" : 11,
+        "name" : "กุ้งถัง - ตลาดอินดี้ ปิ่นเกล้า",
+        "type" : "Coupon, Partner, อาหารทะเล",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000009kf/hero/3cc0e299da694b649e3ff727e05289f9_1601820421725156084.jpeg"
+    },
+    
+    {
+        "id" : 1,
+        "name" : "Potato Corner (โปเตโต้ คอร์เนอร์) - ถนนข้าวสาร",
+        "type" : "Partner, ทานเล่น/ขนมขบเคี้ยว",
+        "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLJEVDYC3JFL6/hero/7cedb192411f4ebeb8cd1c181b3ba35d_1600431327359908312.png"
+    },
+];
+module.exports = data;
